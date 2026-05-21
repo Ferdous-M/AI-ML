a4634a3b5251540b0196c6c08fdd936248f65035
Ferdous-M <mahzabinferdous000@gmail.com>
2026-05-21 12:12:22 +0600

    if - else if

--- python basic/if else.py
+++ python basic/if else.py
@@ -1,5 +1,10 @@
-age = 26
-if age < 26 :
-   print("False")
+age = 25
+
+if age <= 12:
+    print("Child.")
+elif age <= 19:
+    print("Teenager.")
+elif age <= 35:
+    print("Young adult.")
 else:
-   print("true")
+    print("Adult.")
