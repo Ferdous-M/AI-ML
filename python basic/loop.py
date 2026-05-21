81cabb482553621b08bbc4f6735c7c336c0908aa
Ferdous-M <mahzabinferdous000@gmail.com>
2026-05-21 12:16:46 +0600

    while loop

--- python basic/loop.py
+++ python basic/loop.py
@@ -2,6 +2,11 @@
 # for i in range(0,5):
 #     print(i)
 
-a = ["I", "am", "Ferdous-M"]
-for i in range(len(a)):
-    print(a[i])
+# a = ["I", "am", "Ferdous-M"]
+# for i in range(len(a)):
+#     print(a[i])
+
+cnt=0
+while(cnt<3):
+    cnt=cnt+1
+    print(cnt)
