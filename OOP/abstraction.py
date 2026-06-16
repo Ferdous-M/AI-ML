c87b19867d0c35aa4d79ce64c04e3ad423acca2e
Ferdous-M <mahzabinferdous000@gmail.com>
2026-06-16 12:19:42 +0600

    Create abstraction.py

---
+++ OOP/abstraction.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Rectangle(Shape):
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+rect = Rectangle(10, 5)
+
+print(rect.area())
