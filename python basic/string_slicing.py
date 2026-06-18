60074f262c8c8e717fd24d87eee069b4a148edbd
Ferdous-M <mahzabinferdous000@gmail.com>
2026-06-18 12:53:56 +0600

    Update string_slicing.py

--- python basic/string_slicing.py
+++ python basic/string_slicing.py
@@ -1,4 +1,13 @@
 name = "Mahzabin"
 
 print(name[0:8:2])
+#reverse string
 print(name[::-1])
+
+text = "PythonProgramming"
+
+print(text[:6])      # Python
+print(text[6:])      # Programming
+print(text[::2])     # Pto rgamn
+print(text[::-1])    # Reverse string
+print(text[-4:])     # ming
